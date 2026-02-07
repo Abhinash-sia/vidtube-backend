9afa1514265f019f246d3d1300f99c68c543f7e3
Abhinash sia <abhinashsia@gmail.com>
2026-02-07 09:06:37 +0530

    add register controller

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -29,7 +29,7 @@ const userSchema = new Schema({
     requried: true,
   },
   coverImage: {
-    type: string, //cloudinary url
+    type: String, //cloudinary url
   },
   watchHistory: [
     {
@@ -52,7 +52,7 @@ const userSchema = new Schema({
 userSchema.pre("save", async function (next) {
   if(this.isModified("password")) return next ()
 
-  this.password = bcrypt.hash(this.password , 10)
+  this.password = await bcrypt.hash(this.password , 10)
   next()
 } )
 
@@ -81,7 +81,7 @@ userSchema.methods.generateRefreshToken = function(){
     {
       _id : this._id,
       email:this.email,
-      username:this.userName,
+      username:this.username,
       fullname:this.fullname,
     },
     process.env.REFRESH_TOKEN_SECRET,
